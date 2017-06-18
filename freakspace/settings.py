8f8e75c434e84575ef2345a0f0dbb35778aa85ac
xxwolf <xxwolf@users.noreply.github.com>
2017-06-18 12:39:58 +0300

    testing commit

--- freakspace/settings.py
+++ freakspace/settings.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
+
+hello there from xxWolf
+
 """
 
 import os
