b52afdecb624e576d20b33b7abd861973c12bcec
fuckinghacking <navchalkin@ex.ua>
2017-08-13 14:41:31 +0300

    fixed bug with do not aurh user

--- profileuser/views.py
+++ profileuser/views.py
@@ -9,7 +9,10 @@ def out_profile_user(request, id):
         min_5 = 300 # 300 second = 5 min
 
         update_activity(request)
-        user = models.UserProfile.objects.get(user_id=request.user.id)
+        if request.user.is_authenticated():
+            user = models.UserProfile.objects.get(user_id=request.user.id)
+            auth = True
+        # user = models.UserProfile.objects.get(user_id=request.user.id)
         this_user = models.UserProfile.objects.get(id=id)
         # print(models.timezone.now().second - user.last_activity.second)
         if time.time() - this_user.last_activity <= min_5:
