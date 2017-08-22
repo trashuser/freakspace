247ede8d389504432313699a38d901a918d5a5c2
fuckinghacking <navchalkin@ex.ua>
2017-08-22 21:42:10 +0300

    fixed title page, fixed bug with output playlist, add Beta symbol

--- profileuser/views.py
+++ profileuser/views.py
@@ -5,30 +5,30 @@ import time
 
 
 def out_profile_user(request, id):
-    # try:
-        min_5 = 300 # 300 second = 5 min
+    min_5 = 300 # 300 second = 5 min
 
-        update_activity(request)
-        if request.user.is_authenticated():
-            user = models.UserProfile.objects.get(user_id=request.user.id)
-            auth = True
-        # user = models.UserProfile.objects.get(user_id=request.user.id)
-        this_user = models.UserProfile.objects.get(id=id)
-        # print(models.timezone.now().second - user.last_activity.second)
-        if time.time() - this_user.last_activity <= min_5:
-            Online = True
-        else:
-            loctime = time.strftime('%d.%m %H:%M:%S', time.localtime(this_user.last_activity))
-        if request.user.is_authenticated():
-            auth = True
-            if int(request.user.id) == int(id):
-                its_me = True
-        playlists = models.Playlist.objects.filter(author=this_user)
-        posts = models.Post.objects.filter(author=this_user)
-        return render(request, 'profileuser/userpage.html', locals())
-    # except:
-    #     return render(request, 'loginsys/register.html')
+    update_activity(request)
+    if request.user.is_authenticated():
+        user = models.UserProfile.objects.get(user_id=request.user.id)
+        auth = True
+    this_user = models.UserProfile.objects.get(id=id)
+    namepage = this_user.user.username
+    if time.time() - this_user.last_activity <= min_5:
+        Online = True
+    else:
+        loctime = time.strftime('%d.%m %H:%M:%S', time.localtime(this_user.last_activity))
+    if request.user.is_authenticated():
+        auth = True
+        if int(request.user.id) == int(id):
+            its_me = True
+    playlists = models.Playlist.objects.filter(author=this_user)
+    posts = models.Post.objects.filter(author=this_user)
+    return render(request, 'profileuser/userpage.html', locals())
 
+
+
+def edit_profile_user(request, id):
+    pass
 
 
 def update_activity(request):
