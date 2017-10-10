27adb2ae36e0c56791fd8626aee931bc66b17eaf
Rostyslav Golda <tx_hv@ukr.net>
2017-10-10 23:52:18 +0300

    Sraka Doshka bu barah

--- freakspace/settings.py
+++ freakspace/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'freakspace.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'freakspace',
+        'USER': 'freak',
+        'PASSWORD': '598db7ed9457ccb66706a68041435266',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+
     }
 }
 
